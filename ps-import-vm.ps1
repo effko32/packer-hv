b3fd84825208c38ea553c5c031d185ca50248bb2
andrey.deputatov <andrey.deputatov@norbit.ru>
2021-10-18 11:07:50 +0400

    upd-ps-import

--- ps-import-vm.ps1
+++ ps-import-vm.ps1
@@ -1,3 +1,3 @@
 $VMName = "debian"
-Get-ChildItem "C:\github_effko32_packer-hv\$VMName\Virtual Machines\*.vmcx" | Import-VM -Copy -VhdDestinationPath "C:\VirtualMachines\$VMName\Virtual Hard Disks" -VirtualMachinePath "C:\VirtualMachines\$VMName" -GenerateNewId
+Get-ChildItem "C:\packer\$VMName\Virtual Machines\*.vmcx" | Import-VM -Copy -VhdDestinationPath "C:\VirtualMachines\$VMName\Virtual Hard Disks" -VirtualMachinePath "C:\VirtualMachines\$VMName" -GenerateNewId
 Start-VM $VMName
