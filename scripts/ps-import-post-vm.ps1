cde07e986ab6b96643bdfed3c236c3cc95baaad9
andrey.deputatov <andrey.deputatov@norbit.ru>
2022-02-07 18:58:12 +0400

    fix main

--- scripts/ps-import-post-vm.ps1
+++ scripts/ps-import-post-vm.ps1
@@ -1,3 +1,4 @@
-[string] $VMName = $env:VMName
+[System.Threading.Thread]::Sleep(60000)
+string] $VMName = $env:VMName
 Get-ChildItem "C:\packer\$VMName\Virtual Machines\*.vmcx" | Import-VM -Copy -VhdDestinationPath "C:\VirtualMachines\$VMName\Virtual Hard Disks" -VirtualMachinePath "C:\VirtualMachines\$VMName" -GenerateNewId
 Start-VM $VMName
